138408136fe3a24c864fae72c539e72f1118d490
Sowjanya <sowjanyap@chiselontechnologies.com>
2026-02-21 15:39:54 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -25,26 +25,15 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes(
+  const { setColorMode } = useColorModes(
     'coreui-free-react-admin-template-theme'
   )
-  const storedTheme = useSelector((state) => state.theme)
 
+  // ✅ FORCE LIGHT THEME
   useEffect(() => {
     injectTheme()
-  }, [])
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search)
-    const theme = urlParams.get('theme')?.match(/^[A-Za-z0-9\s]+/)?.[0]
-
-    if (theme) {
-      setColorMode(theme)
-    } else if (!isColorModeSet()) {
-      setColorMode(storedTheme)
-    }
-  }, [storedTheme, isColorModeSet, setColorMode])
-
+    setColorMode('light')
+  }, [setColorMode])
   return (
     <BrowserRouter>
       <ToastContainer position="top-right" autoClose={3000} theme="light" />
